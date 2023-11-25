4af1ed09bbc49559d0fb9d1579a6d83310501d33
VodoXleb <sinichkinsergei228@gmail.com>
2023-11-25 18:42:03 +0300

    d
    d

--- handlers.py
+++ handlers.py
@@ -1,9 +1,11 @@
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart, Command, StateFilter
 import copy
 from aiogram import types, Bot, F
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 from loader import dp, msg_router, callback_router, bot
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from random import *
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.utils.keyboard import InlineKeyboardBuilder
@@ -13,16 +15,14 @@ global tasks
 tasks = []
 
 
+class TaskState(StatesGroup):
+    typingName = State()
+    typingDesc = State()
+
 
 class Task:
     name = ''
     desc = ''
-
-global taskNow
-taskNow = Task()
-global requestStatus
-requestStatus = 0
-global msgid
 
 
 @msg_router.message(CommandStart())
@@ -38,10 +38,9 @@ async def write_help(msg: types.Message):
 
 
 @msg_router.message(Command("addtask"))
-async def add_task_init(msg: types.Message):
+async def add_task_init(msg: types.Message, state: FSMContext):
     await msg.answer("Напишите название задачи")
-    global requestStatus
-    requestStatus = 1
+    await state.set_state(TaskState.typingName)
 
 
 @msg_router.message(Command("managetask"))
@@ -56,105 +55,68 @@ async def manage_task(msg: types.Message):
         await msg.answer("Сейчас нет задач")
 
 
-@msg_router.message()
-async def add_task_steps(msg: types.Message):
-    global requestStatus
-    global taskNow
-    if requestStatus == 1:
-        taskNow.name = msg.text
-        await msg.answer("Напишите описание задачи")
-        requestStatus = 2
-    elif requestStatus == 2:
-        taskNow.desc = msg.text
-        msg1 = await msg.answer(f"Задача: {taskNow.name}.\nОписание: {taskNow.desc}.")
-        builder = InlineKeyboardBuilder()
-        builder.button(text="Да", callback_data="accept_task")
-        builder.button(text="Нет", callback_data="reject_task")
-        global msgid
-        msgid = msg1
-        await msg.answer("Вас устраивает это?", reply_markup=builder.as_markup(resize_keyboard=True))
+@msg_router.message(TaskState.typingName)
+async def add_task_steps(msg: types.Message, state: FSMContext):
+    await state.update_data(taskname=msg.text)
+    await msg.answer("Напишите описание задачи")
+    await state.set_state(TaskState.typingDesc)
+
+
+@msg_router.message(TaskState.typingDesc)
+async def add_task_steps(msg: types.Message, state: FSMContext):
+    await state.update_data(taskdesc=msg.text)
+    user_data = await state.get_data()
+    msg1 = await msg.answer(f"Задача: {user_data['taskname']}.\nОписание: {user_data['taskdesc']}.")
+    builder = InlineKeyboardBuilder()
+    builder.button(text="Да", callback_data="accept_task")
+    builder.button(text="Нет", callback_data="reject_task")
+    await msg.answer("Вас устраивает это?", reply_markup=builder.as_markup(resize_keyboard=True))
+    await state.update_data(msgid=msg1)
 
 
 @callback_router.callback_query(F.data == "accept_task")
-async def accept_task(callback: types.CallbackQuery):
-    global taskNow
+async def accept_task(callback: types.CallbackQuery, state: FSMContext):
+    user_data = await state.get_data()
     global tasks
-    global requestStatus
-    tasks.append(copy.deepcopy(taskNow))
-    taskNow.name = ''
-    taskNow.desc = ''
-    requestStatus = 0
-    global msgid
-    await msgid.delete()
+    tasknow = Task()
+    tasknow.name = user_data['taskname']
+    tasknow.desc = user_data['taskdesc']
+    tasks.append(copy.deepcopy(tasknow))
+    await state.clear()
+    await user_data['msgid'].delete()
     await callback.message.delete()
-    #await callback.message.answer("Понял, сохраняю")
 
 
 @callback_router.callback_query(F.data == "reject_task")
-async def reject_task(callback: types.CallbackQuery):
-    global taskNow
-    global requestStatus
-    taskNow.name = ''
-    taskNow.desc = ''
-    requestStatus = 0
-    global msgid
-    await msgid.delete()
+async def reject_task(callback: types.CallbackQuery, state: FSMContext):
+    user_data = await state.get_data()
+    await user_data['msgid'].delete()
+    await state.clear()
     await callback.message.delete()
-    #await callback.message.answer("Понял, отмена")
 
 
 @callback_router.callback_query(F.data.find('manage') != -1)
-async def manage_task(callback: types.CallbackQuery):
+async def manage_task(callback: types.CallbackQuery, state: FSMContext):
     i = int((callback.data.split('/'))[1])
     global tasks
     builder = InlineKeyboardBuilder()
     builder.button(text="Удалить", callback_data=f"delete/{i}")
     builder.button(text="Не удалять", callback_data="dont delete")
-    global msgid
-    msgid = await callback.message.answer(f"Задача \n Название: {tasks[i].name} \n Описание: {tasks[i].desc}", reply_markup=builder.as_markup(resize_keyboard=True))
+    msgid = await callback.message.answer(f"Задача \n Название: {tasks[i].name} \n Описание: {tasks[i].desc}",
+                                          reply_markup=builder.as_markup(resize_keyboard=True))
+    await state.update_data(msgid=msgid)
 
 
 @callback_router.callback_query(F.data.startswith('delete'))
-async def reject_task(callback: types.CallbackQuery):
+async def reject_task(callback: types.CallbackQuery, state: FSMContext):
     global tasks
     i = int((callback.data.split('/'))[1])
     tasks.pop(i)
-    global msgid
-    await msgid.delete()
+    user_data = await state.get_data()
+    await user_data['msgid'].delete()
 
 
 @callback_router.callback_query(F.data == "dont delete")
-async def reject_task(callback: types.CallbackQuery):
-    global msgid
-    await msgid.delete()
-
-
-# @msg_router.message(Command("random_generator"))
-# async def reply_build(msg: types.Message):
-#     builder = InlineKeyboardBuilder()
-#     builder.button(text="Нажми меня", callback_data="random_number")
-#     builder.adjust(3)
-#     await msg.answer("Кнопка отправки случайного числа:", reply_markup=builder.as_markup(resize_keyboard=True))
-#
-#
-# @callback_router.callback_query(F.data == "random_number")
-# async def random_number(callback: types.CallbackQuery):
-#     await callback.message.answer(str(randint(1, 10)))
-#
-#
-# @msg_router.message()
-# async def echo_handler(msg: types.Message) -> None:
-#     try:
-#         if msg.text == "Убери клавиатуру":
-#             await msg.reply("Понял, убираю", reply_markup=types.ReplyKeyboardRemove())
-#         else:
-#                 await msg.reply(f"{msg.text}, отлично")
-#     except TypeError:
-#         await msg.answer("Не понял")
-#
-#
-# @msg_router.message()
-# async def message_handler(message: Message) -> None:
-#     await message.answer('Hello from my router!')
-#
-
+async def reject_task(callback: types.CallbackQuery, state: FSMContext):
+    user_data = await state.get_data()
+    await user_data['msgid'].delete()
